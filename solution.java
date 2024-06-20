2161fa8a569c3cae168f06c12a90b04bfa7d6c95
Kooreis <kooreis.official@gmail.com>
2024-06-19 20:30:48 -0500

    In this update, the Scanner class is imported and the main method is created. The user is prompted to enter the number of strings they will input.

--- solution.java
+++ solution.java
@@ -1,4 +1,3 @@
-```java
 import java.util.Scanner;
 
 public class Main {
@@ -7,23 +6,5 @@ public class Main {
         System.out.println("Enter the number of strings:");
         int n = scanner.nextInt();
         scanner.nextLine();
-        String[] strs = new String[n];
-        System.out.println("Enter the strings:");
-        for (int i = 0; i < n; i++) {
-            strs[i] = scanner.nextLine();
-        }
-        System.out.println("Longest Common Prefix: " + longestCommonPrefix(strs));
-    }
-
-    public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
-            }
-        return prefix;
     }
 }
-```
