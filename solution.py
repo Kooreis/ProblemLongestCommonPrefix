827b45214b57b692124d97d8b22eebfe63e7aed4
Kooreis <kooreis.official@gmail.com>
2024-06-19 20:30:48 -0500

    This is the function that computes the longest common prefix among a list of strings. It first checks if the list is empty, and if so, returns an empty string. Then it finds the shortest string in the list, and iterates over its characters. For each character, it checks if it is the same in all other strings. If it finds a character that is not the same in all strings, it returns the prefix up to that character. If it doesn't find such a character, it returns the whole shortest string, which is the common prefix.

--- solution.py
+++ solution.py
@@ -1,6 +1,3 @@
-Here is a Python console application that computes the longest common prefix among a list of strings:
-
-```python
 def longest_common_prefix(strs):
     if not strs:
         return ""
@@ -10,13 +7,3 @@ def longest_common_prefix(strs):
             if other[i] != char:
                 return shortest_str[:i]
     return shortest_str
-
-def main():
-    strs = input("Enter strings separated by space: ").split()
-    print("Longest common prefix: ", longest_common_prefix(strs))
-
-if __name__ == "__main__":
-    main()
-```
-
-You can run this application in the console. It will ask you to enter strings separated by space. After you enter the strings and press enter, it will print the longest common prefix among the entered strings.
