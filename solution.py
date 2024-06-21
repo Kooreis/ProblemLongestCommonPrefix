8e48252a544e6b728a4e726a18ad3ff84d5ab0e8
Kooreis <kooreis.official@gmail.com>
2024-06-20 20:30:48 -0500

    This is the main function that gets the strings from the user, calls the longest_common_prefix function with these strings, and prints the result. It first asks the user to enter strings separated by space, and splits the input string into a list of strings. Then it calls the longest_common_prefix function with this list, and prints the result.

--- solution.py
+++ solution.py
@@ -1,9 +1,3 @@
-def longest_common_prefix(strs):
-    if not strs:
-        return ""
-    shortest_str = min(strs, key=len)
-    for i, char in enumerate(shortest_str):
-        for other in strs:
-            if other[i] != char:
-                return shortest_str[:i]
-    return shortest_str
+def main():
+    strs = input("Enter strings separated by space: ").split()
+    print("Longest common prefix: ", longest_common_prefix(strs))
