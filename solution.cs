ca760d73c40cb477247cc760c00827e583b951e9
Kooreis <kooreis.official@gmail.com>
2024-06-20 20:30:48 -0500

    Created the main program class and the main method. Initialized a list of strings and printed the longest common prefix of the list.

--- solution.cs
+++ solution.cs
@@ -1,2 +1,7 @@
-using System;
-using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        List<string> strings = new List<string> { "flower", "flow", "flight" };
+        Console.WriteLine(LongestCommonPrefix(strings));
+    }
